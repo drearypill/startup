3c5df11b96a1f4c13b4ecbf2a55ca7cfab2e4129
drearypill <lilystyle100@gmail.com>
2023-06-11 20:11:09 -0700

    fixes to db js

--- database.js
+++ database.js
@@ -1,9 +1,12 @@
 const { MongoClient } = require("mongodb");
+const bcrypt = require("bcrypt");
+const uuid = require("uuid");
 const config = require("./dbConfig.json");
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db("startup");
+const userCollection = db.collection("user");
 const playerCollection = db.collection("player");
 
 // This will asynchronously test the connection and exit the process if it fails

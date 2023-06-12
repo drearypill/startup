bff1f9561199ef3839821b5eb60fb08abdd6f748
drearypill <lilystyle100@gmail.com>
2023-06-11 18:44:44 -0700

    database.js functions updated for player access

--- database.js
+++ database.js
@@ -1,33 +1,31 @@
-const { MongoClient } = require('mongodb');
-const config = require('./dbConfig.json');
+const { MongoClient } = require("mongodb");
+const config = require("./dbConfig.json");
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('simon');
-const scoreCollection = db.collection('score');
+const db = client.db("startup");
+const playerCollection = db.collection("player");
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
   await client.connect();
   await db.command({ ping: 1 });
 })().catch((ex) => {
-  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  console.log(
+    `Unable to connect to database with ${url} because ${ex.message}`
+  );
   process.exit(1);
 });
 
-async function addScore(score) {
-  const result = await scoreCollection.insertOne(score);
+async function addPlayer(player) {
+  const result = await playerCollection.insertOne(player);
   return result;
 }
 
-function getHighScores() {
-  const query = { score: { $gt: 0, $lt: 900 } };
-  const options = {
-    sort: { score: -1 },
-    limit: 10,
-  };
-  const cursor = scoreCollection.find(query, options);
-  return cursor.toArray();
+async function getPlayers() {
+  const cursor = playerCollection.find();
+  const list = await cursor.toArray();
+  return list.map(getName);
 }
 
-module.exports = { addScore, getHighScores };
+module.exports = { addPlayer, getPlayers };
